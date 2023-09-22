9cc1c91bde47226c5714593288b71e8913cb1149
duc.trandinh <duc.trandinh@vti.com.vn>
2023-09-22 16:52:11 +0700

    Revert "Create nimbus generator"
    This reverts commit 89c453e1df6a85632aa69520f9c92bd62bb9f16f.

--- nimbus_annotation/lib/nimbus_annotation.dart
+++
@@ -1,4 +0,0 @@
-library nimbus_annotation;
-
-export 'src/nimbus_api.dart';
-export 'src/dataset.dart';
--- nimbus_generator/lib/builder.dart
+++
@@ -1,6 +0,0 @@
-import 'package:build/build.dart';
-import 'package:source_gen/source_gen.dart';
-import 'package:nimbus_generator/src/nimbus_generator.dart';
-
-Builder nimbusSupporter(BuilderOptions options) =>
-    SharedPartBuilder([NimbusSupporterGenerator()], 'nimbus_annotation');
