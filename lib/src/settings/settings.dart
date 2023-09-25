9982fe516b933abd163e3e812cdb1760b75f745b
duchdtran <duchdtran2706@gmail.com>
2023-09-25 13:53:40 +0700

    Upgrade library

--- lib/src/settings/settings.dart
+++ lib/src/settings/settings.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
+import 'package:nimbus4flutter/nimbus4flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import '../../nimbus4flutter.dart';
 
 Future<T?> loadSetting<T>(String name, T setting) async{
   final SharedPreferences prefs = await SharedPreferences.getInstance();
